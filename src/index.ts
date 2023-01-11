2336b8fd1a9794efbbfee7a8b3c36dc877116ce1
Harun1804 <Harun.arrasyid1804@gmail.com>
2023-01-11 17:47:50 +0700

    Prettier config

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,9 @@ const app: Application = express();
 const port: Number = 3000;
 
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
-    res.send('Hello World')
-})
+  res.send('Hello World');
+});
 
 app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
-})
+  console.log(`Server is running on port: ${port}`);
+});
