42dca8bcd17d7778f238c28820edc01125695913
Harun1804 <Harun.arrasyid1804@gmail.com>
2023-01-11 17:20:34 +0700

    Initial

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express, { Application, Request, Response, NextFunction } from 'express';
+
+const app: Application = express();
+const port: Number = 3000;
+
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+    res.send('Hello World')
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+})
